e15d69617a7ca3ed1be1759fb2d20467ab0a8e18
Jason Cox <coxj@vt.edu>
2024-05-03 12:20:15 -0400

    assessmentviews

---
+++ WinsorApps.MAUI.THEAssessmentCalendar/Views/AssessmentView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinsorApps.MAUI.THEAssessmentCalendar.Views;
+
+public partial class AssessmentView : ContentView
+{
+    public AssessmentView()
+    {
+        InitializeComponent();
+    }
+}
