89101be350eee5f2d22c6621244764eebb119643
Jason Cox <coxj@vt.edu>
2024-05-13 09:59:59 -0400

    LoginPage update

--- WinsorApps.MAUI.THEAssessmentCalendar/MainPage.xaml.cs
+++ WinsorApps.MAUI.THEAssessmentCalendar/MainPage.xaml.cs
@@ -1,23 +1,23 @@
-﻿namespace WinsorApps.MAUI.THEAssessmentCalendar;
+﻿using WinsorApps.MAUI.Shared.Pages;
+using WinsorApps.MAUI.Shared.ViewModels;
+using WinsorApps.Services.Global.Services;
+
+namespace WinsorApps.MAUI.THEAssessmentCalendar;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
-    public MainPage()
+    public MainPage(RegistrarService registrar, ApiService api, LocalLoggingService logging)
     {
+        MainPageViewModel vm = new(
+        [
+            new(registrar, "Registrar Data")
+        ]);
+        BindingContext = vm;
+
+        LoginPage loginPage = new(logging, vm.LoginVM);
+        loginPage.OnLoginComplete += (_, _) => Navigation.PopAsync();
+
+        Navigation.PushAsync(loginPage);
         InitializeComponent();
-    }
-
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
     }
 }
