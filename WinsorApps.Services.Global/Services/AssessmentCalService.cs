e18dbe45ad5c9ab55119d67739b9676aee9f0998
Jason Cox <coxj@vt.edu>
2024-05-17 11:33:01 -0400

    Cache for Assessments and Late Passes (Lara)

--- WinsorApps.Services.Global/Services/AssessmentCalService.cs
+++ WinsorApps.Services.Global/Services/AssessmentCalService.cs
@@ -34,13 +34,50 @@ public class AssessmentCalService : IAsyncInitService
             $"api/assessment-calendar/my-calendar");
     }
     
-    // Use late pass command 
+    /// <summary>
+    /// My Assessments Cache.
+    /// </summary>
+    private ImmutableArray<AssessmentRecords.Assessment> _myAssessments;
+    
+    /// <summary>
+    /// Access My Assessments cache.
+    /// </summary>
+    public ImmutableArray<AssessmentRecords.Assessment> MyAssessments => _myAssessments;
 
-    public async Task<ImmutableArray<AssessmentRecords.LatePass>> PostLatePass(bool? passUsed, bool? passAvailable)
+    public async Task<ImmutableArray<AssessmentRecords.Assessment>> GetAssessmentsAsync()
+    {
+        return await _api.SendAsync<ImmutableArray<AssessmentRecords.Assessment>>(HttpMethod.Get, "api/assessment-calendar/my-calendar");
+    }
+
+    public async Task GetAssessmentsAsync(ImmutableArray<AssessmentRecords.Assessment> _myAssessments)
+    {
+        this._myAssessments = await GetAssessmentsAsync();
+    }
+    /// <summary>
+    /// My Late Pass Cache. 
+    /// </summary>
+    private ImmutableArray<AssessmentRecords.LatePass> _myLatePasses;
+
+    /// <summary>
+    /// Access My Late Passes cache. 
+    /// </summary>
+
+    public ImmutableArray<AssessmentRecords.LatePass> MyLatePasses => _myLatePasses;
+
+    public async Task<ImmutableArray<AssessmentRecords.LatePass>> GetLatePassesAsync()
+    {
+        return await _api.SendAsync<ImmutableArray<AssessmentRecords.LatePass>>(HttpMethod.Get,
+            "api/assessment-calendar/students/passes?showPast=true");
+    }
+    public async Task GetLatePassesAsync(ImmutableArray)
+    
+    // Use late pass command 
+    public async Task<ImmutableArray<AssessmentRecords.LatePass>> PostLatePass(string id)
     {
         return await _api.SendAsync<ImmutableArray<AssessmentRecords.LatePass>>(HttpMethod.Post,
-            $"api/assessment-calendar/students/passes?showPast=true");
+            $"api/assessment-calendar/students/passes/{id}");
     }
+    
         
     // Withdraw late pass - what http method would I use here? (delete?)
 
@@ -59,6 +96,8 @@ public class AssessmentCalService : IAsyncInitService
         // Initialize your cache here~
         // await any api calls that you want
         // to populate in memory.
+       
+        _myAssessments = await GetAssessmentsAsync();
 
         Progress = 1;
         Ready = true;
