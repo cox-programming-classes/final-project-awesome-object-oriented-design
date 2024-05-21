46d876adfb3c0f4fbd1bc078511c5f402b86345c
Jason Cox <coxj@vt.edu>
2024-05-21 10:08:25 -0400

    some fixes and comments!

--- WinsorApps.MAUI.THEAssessmentCalendar/ViewModels/CalendarViewModel.cs
+++ WinsorApps.MAUI.THEAssessmentCalendar/ViewModels/CalendarViewModel.cs
@@ -1,32 +1,50 @@
 using System.Collections.Immutable;
 using System.Globalization;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using WinsorApps.Services.Global;
 using WinsorApps.Services.Global.Services;
 
 namespace WinsorApps.MAUI.THEAssessmentCalendar.ViewModels;
 
 public partial class CalendarViewModel : ObservableObject
 {
-    private readonly AssessmentCalService _Calendar;
+    private readonly AssessmentCalService _calendar;
     [ObservableProperty] private DateTime day;
     [ObservableProperty] private ImmutableArray<DayViewModel> days;
+    
+    // consider adding properties for things like IsMonthlyView or IsWeeklyView
+    // and a private field that holds all the assessments that you have cached
 
     public CalendarViewModel(AssessmentCalService calendar)
     {
-        _Calendar = calendar;
+        _calendar = calendar;
         day = DateTime.Today;
-        var start = new(day.Year, day.Month, 1);
+        var start = new DateTime(day.Year, day.Month, 1);
         var end = start.AddMonths(1).AddDays(-1);
-        calendar.GetAssessmentsByDate(start, end);
+        var task = calendar.GetAssessmentsByDate(start, end);
+        task.WhenCompleted(() =>
+        {
+            var assessments = task.Result;
+            // initialize Days with all the ViewModels in this list that
+            // should be displayed in the view.
+        });
     }
 
+    [RelayCommand]
     public void PlusOneMonth()
     {
-        day = day.AddMonths(1);
+        Day = Day.AddMonths(1);
+        // update the Days collection with new stuff
+        // that might entail making this an async Task
+        // also each of these needs to be a Relay Command as well~
     }
 
     public void MinusOneMonth()
     {
+        // use the capital letter Day variables~
+        // using the lowercase version skips all the 
+        // Observable Property magic
         day = day.AddMonths(-1);
     }
 
